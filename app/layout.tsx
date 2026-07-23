a066697f43f18aee8d759b87d9863134ed3c5b0d
hmsuiji <1704802092@qq.com>
2026-07-23 19:00:00 +0800

    feat: oxc and heroui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "@/assets/styles/globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
-      lang="en"
+      lang="zh-CN"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
-  typedRoutes: true,
-  experimental: {
-    useTypeScriptCli: true,
-  },
+	/* config options here */
+	reactCompiler: true,
+	typedRoutes: true,
+	experimental: {
+		useTypeScriptCli: true,
+		turbopackRustReactCompiler: true,
+	},
 };
 
 export default nextConfig;
