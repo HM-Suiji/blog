769ecf1e47862b65c2f03e268c8c34c6ca5824c2
hmsuiji <1704802092@qq.com>
2026-07-23 19:53:21 +0800

    feat: drizzle

---
+++ config/site.ts
@@ -0,0 +1,36 @@
+export const siteConfig = {
+  name: '穗积的宇宙船',
+  description:
+    '一个普普通通的程序员将与大家在这个宇宙船里分享一些开发经验和生活点滴。',
+  author: 'HM-Suiji',
+  links: {
+    github: 'https://github.com/HM-Suiji',
+  },
+  nav: [
+    { label: '博客', href: '/posts' },
+    {
+      label: '友链',
+      href: '/friends',
+    },
+    {
+      label: '关于',
+      href: '/about',
+      children: [
+        {
+          label: '留言板',
+          href: '/board',
+        },
+        {
+          label: '装备库',
+          href: '/equipments',
+        },
+        {
+          label: '项目集',
+          href: '/projects',
+        },
+      ],
+    },
+  ],
+} as const
+
+export type SiteConfig = typeof siteConfig
---
+++ server/db/schema/post.ts
@@ -0,0 +1,28 @@
+import { sql } from 'drizzle-orm'
+import { pgTable, uuid, timestamp, varchar, integer } from 'drizzle-orm/pg-core'
+
+export const postsTable = pgTable('posts', {
+  id: uuid()
+    .primaryKey()
+    .default(sql`uuidv7()`),
+  title: varchar('title', { length: 256 }).notNull(),
+  slug: varchar('slug', { length: 256 }).unique().notNull(),
+  description: varchar('description', { length: 256 }),
+  author: varchar('author', { length: 256 }).notNull(),
+  cover: varchar('cover', { length: 256 }),
+  publishedAt: timestamp('published_at', { mode: 'date' })
+    .defaultNow()
+    .notNull(),
+  updatedAt: timestamp('updated_at', { mode: 'date' }).defaultNow().notNull(),
+  contentPath: varchar('content_path', { length: 256 }).notNull(),
+})
+
+export const postStatsTable = pgTable('post_stats', {
+  id: uuid()
+    .primaryKey()
+    .default(sql`uuidv7()`),
+  postId: uuid().references(() => postsTable.id),
+  views: integer('views').default(0).notNull(),
+  likes: integer('likes').default(0).notNull(),
+  averageReadTime: integer('average_read_time').default(0).notNull(),
+})
