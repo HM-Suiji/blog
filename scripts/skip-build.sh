2fccdfc700c1eb03f528f9ca2f26264b74b6177c
hmsuiji <1704802092@qq.com>
2026-08-06 22:53:47 +0800

    chore

--- scripts/skip-build.sh
+++ scripts/skip-build.sh
@@ -1,1 +1,1 @@
-if [ "$VERCEL_GIT_COMMIT_REF" != "prod" ]; then echo "Skipping deployment for non-prod branch: $VERCEL_GIT_COMMIT_REF"; exit 0; fi
+if [ "$VERCEL_GIT_COMMIT_REF" != "main" ]; then echo "Skipping deployment for non-main branch: $VERCEL_GIT_COMMIT_REF"; exit 0; fi
