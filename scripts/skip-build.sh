a1c8a08bd72f430ea02c39af8921ee32cc42ec32
hmsuiji <1704802092@qq.com>
2026-08-06 22:07:27 +0800

    chore: remove skip deploy by custom files

--- scripts/skip-build.sh
+++ scripts/skip-build.sh
@@ -1,11 +1,3 @@
 if [ "$VERCEL_GIT_COMMIT_REF" != "main" ]; then
   exit 1
 fi
-
-CHANGED=$(git diff --name-only $VERCEL_GIT_PREVIOUS_SHA $VERCEL_GIT_COMMIT_SHA)
-
-if echo "$CHANGED" | grep -qvE '^(content/|\.github/|\.vscode/|\.agents/|skills/|public/|\.pages.yml)'; then
-  exit 1
-else
-  exit 0
-fi
