4db026777875eee3e632fdaa6e9b1d1bfd915892
hmsuiji <1704802092@qq.com>
2026-08-13 14:12:33 +0800

    feat: 更新认证布局和登录页面，添加卡片组件以改善用户体验

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,9 +1,22 @@
+import { Card } from '@heroui/react'
+
 import { GithubOAuth } from '@/components/auth/oauth-button'
 
 export default function LoginPage() {
   return (
     <>
-      <GithubOAuth />
+      <Card className="py-12 px-20">
+        <Card.Header className="mx-auto">
+          <Card.Title className="text-3xl mb-4">欢迎你的登录</Card.Title>
+          <Card.Description>登陆以获取更多功能</Card.Description>
+        </Card.Header>
+        <Card.Content className="mt-4 mx-auto">
+          <div className="flex flex-col gap-2">
+            <p>使用第三方账号登录</p>
+            <GithubOAuth />
+          </div>
+        </Card.Content>
+      </Card>
     </>
   )
 }
